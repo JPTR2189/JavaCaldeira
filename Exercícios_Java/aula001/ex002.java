1856ea99b85f512024218d8fcf58627d2bbc4e0d
Jean Pierre <jptr2189@gmail.com>
2023-10-03 17:50:37 -0300

    Exercicio Ex002 feito

---
+++ Exercícios_Java/aula001/ex002.java
@@ -0,0 +1,18 @@
+package JavaCaldeira.Exercícios_Java.aula001;
+import java.util.Scanner;
+public class ex002 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Digite um número: ");
+        String n2 = scanner.nextLine();
+
+        System.out.printf("Digite outro número: ");
+        String n1 = scanner.nextLine();
+
+        System.out.printf("Os número digitados foram: " + n1 + " e " + n2);
+
+
+    }
+}
