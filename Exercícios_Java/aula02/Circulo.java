012821291f679b68ab9a82e30b412f0c3435e3fd
Jean Pierre <jptr2189@gmail.com>
2023-10-11 15:25:56 -0300

    Exercício Ex002 da Aula02 feito

--- Exercícios_Java/aula02/Circulo.java
+++ Exercícios_Java/aula02/Circulo.java
@@ -1,2 +1,64 @@
-package JavaCaldeira.Exercícios_Java.aula02;public class Circulo {
+package JavaCaldeira.Exercícios_Java.aula02;
+
+import java.text.DecimalFormat;
+
+public class Circulo {
+
+    DecimalFormat formatador = new DecimalFormat("#.##");
+    private double raio;
+
+    public Circulo(double raio){
+
+        this.raio = raio;
+
+    }
+
+    public void calcularArea(){
+        double area = 3.14 * (raio * raio) ;
+
+        System.out.println("------------------------------------");
+        System.out.println("A área do círculo é: " + formatador.format(area));
+        System.out.println("------------------------------------");
+    }
+
+    public void calcularCircufenrencia(){
+       double circunferencia = (2 * 3.14) * (raio * raio);
+        System.out.println("------------------------------------");
+        System.out.println("A circunferência do círculo: " + formatador.format(circunferencia));
+        System.out.println("------------------------------------");
+
+    }
+
+    public void calcularDiametro(){
+        double diametro = raio * 2;
+        System.out.println("------------------------------------");
+        System.out.println("O diâmetro do círculo é: " + formatador.format(diametro));
+        System.out.println("------------------------------------");
+    }
+
+    public void compararDoisCirculos(double circulo1, double circulo2){
+
+        if (circulo1 > circulo2){
+            System.out.println("------------------------------------");
+            System.out.println("O Círculo 1 tem o raio maior (" + circulo1 + ")");
+            System.out.println("------------------------------------");
+        }
+        else if (circulo2 > circulo1){
+            System.out.println("------------------------------------");
+            System.out.println("O Círculo 2 tem o raio maior (" + circulo2 + ")");
+            System.out.println("------------------------------------");
+    }    else
+        {
+            System.out.println("------------------------------------");
+            System.out.println("Os dois círculos tem o raio igual");
+            System.out.println("------------------------------------");
+        }
+    }
+
+    public void definirRaio(double raio){
+
+        this.raio = raio;
+
+    }
+
 }
--- Exercícios_Java/aula02/ex002.java
+++ Exercícios_Java/aula02/ex002.java
@@ -1,2 +1,80 @@
-package JavaCaldeira.Exercícios_Java.aula02;public class ex002 {
+package JavaCaldeira.Exercícios_Java.aula02;
+import javax.naming.directory.SchemaViolationException;
+import java.util.Scanner;
+
+
+public class ex002 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Digite o raio do círculo: ");
+        double raio = scanner.nextDouble();
+
+
+        Circulo circulo = new Circulo(raio);
+
+        while (true) {
+            System.out.println("------------------------------------");
+
+            System.out.printf("Escolha uma opção:\n" +
+                    "0 - Sair do programa\n" +
+                    "1 - Calcular a área\n" +
+                    "2 - Calcular a circunferência\n" +
+                    "3 - Calcular diâmetro do círculo\n" +
+                    "4 - Comparar 2 círculos\n" +
+                    "5 - Definir novo raio\n");
+
+            System.out.println("------------------------------------");
+
+
+            int opcao = scanner.nextInt();
+
+            if (opcao == 0) {
+                System.out.println("------------------------------------");
+                System.out.println("Até logo!");
+                System.out.println("------------------------------------");
+                break;
+            }
+
+            switch (opcao){
+
+                case 1:
+                    circulo.calcularArea();
+                    break;
+
+                case 2:
+                    circulo.calcularCircufenrencia();
+                    break;
+
+                case 3:
+                    circulo.calcularDiametro();
+                    break;
+
+                case 4:
+                    System.out.println("Raio do círculo 1:");
+                    double circulo1 = scanner.nextInt();
+
+                    System.out.println("Raio do círculo 2: ");
+                    double circulo2 = scanner.nextInt();
+
+                    circulo.compararDoisCirculos(circulo1, circulo2);
+
+                    break;
+
+                case 5:
+                    System.out.printf("Digite o raio do círculo: ");
+                    double novoRaio = scanner.nextDouble();
+                    circulo.definirRaio(novoRaio);
+
+                    break;
+
+            }
+
+        }
+
+
+
+    }
 }
