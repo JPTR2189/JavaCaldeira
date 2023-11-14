5aac8f714dda4be9f7e5fdcb6b552dce68d22ceb
Jean Pierre <jptr2189@gmail.com>
2023-11-13 22:04:22 -0200

    Ex003 aula03 feito

---
+++ Exercícios_Java/aula03/parte1/ex003/Tarefa.java
@@ -0,0 +1,2 @@
+package JavaCaldeira.Exercícios_Java.aula03.parte1.ex003;public class Tarefa {
+}
---
+++ Exercícios_Java/aula03/parte1/ex003/ex003.java
@@ -0,0 +1,8 @@
+package JavaCaldeira.Exercícios_Java.aula03.parte1;
+
+
+// Criar uma lista de tarefas com ArrayList
+// Deve conter metódos como adicionar, remover e exbir tarefas
+// Criar uma classe com nome "Tarefa"
+public class ex003 {
+}
